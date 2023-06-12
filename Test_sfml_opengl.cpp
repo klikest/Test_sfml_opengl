8de17826c9be2d4c289bfe44febe5a8990e3b039
klikest <klikest@bk.ru>
2023-06-12 05:21:16 +0300

    Result

--- Test_sfml_opengl.cpp
+++ Test_sfml_opengl.cpp
@@ -1,52 +1,124 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
-
 #include<windows.h>
 
+#include <cmath>                                
+
+#define PI 3.14159265   
+
+struct Point
+{
+    int x, y;
+    sf::RectangleShape point;
+
+    Point(int x_, int y_)
+    {
+        x = x_;
+        y = y_;
+        point.setOrigin(sf::Vector2f(0, 0));
+        point.setSize(sf::Vector2f(4.5, 4.5));
+        point.setPosition(sf::Vector2f(x, y));
+    }
+
+    void set_pos_point(int x_, int y_)
+    {
+        point.setPosition(sf::Vector2f(x_, y_));
+    }    
+};
+
+
+struct Grid
+{
+    int x0, y0;
+    int size_x, size_y;
+    std::vector<Point> grid;
+
+    Grid(int x0_, int y0_, int size_x_, int size_y_)
+    {
+        x0 = x0_;
+        y0 = y0_;
+        size_x = size_x_;
+        size_y = size_y_;
+        for (int i = 0; i < size_x; i++)
+        {
+            for (int j = 0; j < size_y; j++)
+            {
+                Point crt_p(x0 +i*5, y0+j*5);
+                grid.push_back(crt_p);
+            }
+        }
+    }
+    void move_grid(int x_, int y_)
+    {
+        int x0_ = grid[0].x;
+        int y0_ = grid[0].y;
+
+        for (int i = 0; i < grid.size(); i++)
+        {
+            int dx = grid[i].x - x0_;
+            int dy = grid[i].y - y0_;
+            grid[i].x = x_ + dx;
+            grid[i].y = y_ + dy;
+            grid[i].set_pos_point(grid[i].x, grid[i].y);
+        }
+
+    }
+
+    void render_grid(sf::RenderWindow &window)
+    {
+        for (int i = 0; i < grid.size(); i++)
+        {
+            window.draw(grid[i].point);
+        }
+    }
+    void change_color()
+    {
+        for (int i = 0; i < grid.size(); i++)
+        {
+            grid[i].point.setFillColor(sf::Color::Green);
+        }
+    }
+
+    void boolean_cut(Grid tool)
+    {
+        for (int i = 0; i < tool.grid.size(); i++)
+        {
+            for (int j = 0; j < grid.size(); j++)
+            {
+                if (tool.grid[i].x == grid[j].x && tool.grid[i].y == grid[j].y)
+                {
+                    grid[j].point.setSize(sf::Vector2f(0, 0));
+                }
+            }
+        }
+    }
+
+};
 
 
 int main()
 {
 
-    const int grid_x = 10;
-    const int grid_y = 10;
-
     int x = 500;
     int y = 500;
 
-
-    sf::RectangleShape move_rec;
-    move_rec.setSize(sf::Vector2f(10, 10));
-    move_rec.setOrigin(sf::Vector2f(0, 0));
-    move_rec.setPosition(sf::Vector2f(x, y));
-
-    sf::RectangleShape grid[grid_x][grid_y];
-
-    for (int i = 0; i < grid_x; i++)
-    {
-        for (int j = 0; j < grid_y; j++)
-        {
-            grid[i][j].setSize(sf::Vector2f(10, 10));
-            grid[i][j].setOrigin(sf::Vector2f(0, 0));
-            grid[i][j].setPosition(sf::Vector2f(100 + 20*i, 100 + 20*j));
-        }
-    }
-
     // Создаем главное окно приложения
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Test");
-    sf::RectangleShape rectangle;
+    sf::RenderWindow window(sf::VideoMode(800, 640), "SFML Test");
+
+    Grid blank(50, 50, 130, 70);
     
-    sf::Text message;
+    Grid tool(x, y, 11, 11);
+    tool.change_color();
+
     sf::Font font;
     font.loadFromFile("arial.ttf");
-    message.setFont(font);
-    message.setString("0");
-    message.setCharacterSize(40);
-    message.setFillColor(sf::Color::White);
-
+    sf::Text text_fps;
+    text_fps.setFont(font);
+    text_fps.setCharacterSize(20);
+    text_fps.setFillColor(sf::Color::White);
 
     sf::Clock clock;
-    float time_end = clock.getElapsedTime().asMilliseconds();
+    float time_end = clock.getElapsedTime().asSeconds();
 
     // Главный цикл приложения
     while (window.isOpen())
@@ -61,47 +133,34 @@ int main()
                 (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
                 window.close();
 
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-            {
-                x -= 10;
-                move_rec.setPosition(sf::Vector2f(x, y));
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-            {
-                x += 10;
-                move_rec.setPosition(sf::Vector2f(x, y));
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-            {
-                y -= 10;
-                move_rec.setPosition(sf::Vector2f(x, y));
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-            {
-                y += 10;
-                move_rec.setPosition(sf::Vector2f(x, y));
-            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) { x -= 5; }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) { x += 5; }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) { y -= 5;}
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) { y += 5;}
+            //std::cout << "x = " << x << "   y = " << y << std::endl;
         }
         
-        Sleep(21);
-
-        // Очистка
         window.clear();
-        // Тут будут вызываться функции обновления и отрисовки объектов
-        // Отрисовка
-        window.draw(message);
-        for (int i = 0; i < grid_x; i++)
-        {
-            for (int j = 0; j < grid_y; j++)
-            {
-                window.draw(grid[i][j]);
-            }
-        }
-        window.draw(move_rec);
-        window.display();
+
+      
+        tool.move_grid(x, y);
+        blank.boolean_cut(tool);
+
+        blank.render_grid(window);
+        tool.render_grid(window);
+
         time_end = clock.getElapsedTime().asSeconds();
-        message.setString(std::to_string((int) round(1/time_end)));
+        int FPS = (int)(1 / time_end);
+        std::string str = std::to_string(FPS);
+        text_fps.setString(str);
+        window.draw(text_fps);
+        //std::cout <<"FPS = " << (int)(1 / time_end) << std::endl;
+        
+
+        window.display();
+        
     }
+
 
     return 0;
 }
